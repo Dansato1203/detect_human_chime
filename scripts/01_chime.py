011a8cf66d571167a4ceb3742466013d3f8a399c
Dan Sato <s19C1054BU@s.chibakoudai.jp>
2021-01-05 23:22:42 +0900

    update

--- scripts/01_chime.py
+++ scripts/01_chime.py
@@ -3,20 +3,32 @@
 
 import rospy
 import rosnode
+import message_filters
 from playsound import playsound
 from std_msgs.msg import Int32
 
-def callback(data):
-  print('お客様が{0}名でご来店されました。'.format(data.data))
-  playsound("konnbini.mp3")
-
+def callback(msg1, msg2):
+  det_face = msg1.data
+  if det_face == 1:
+    print('お客様が{0}名でご来店されました。'.format(msg2.data))
+    playsound("konnbini.mp3")
+    rospy.sleep(10)
 
 def main():
   rospy.init_node("chime_no1_node")
 
   print("お客様がご来店されたら入店音を鳴らします。")
   
-  sub = rospy.Subscriber("/detect_face", Int32, callback)
+  sub1 = message_filters.Subscriber('detect_face', Int32)
+  sub2 = message_filters.Subscriber('number_of_face', Int32)
+
+  queue_size = 1
+  fps = 100.
+  delay = 1 / fps * 0.5
+
+  mf = message_filters.ApproximateTimeSynchronizer([sub1, sub2], queue_size, delay, allow_headerless=True)
+  mf.registerCallback(callback)
+
   rospy.spin()
 
 
--- scripts/detect_human.py
+++ scripts/detect_human.py
@@ -5,8 +5,6 @@ import sys
 import rospy
 import cv2
 import numpy as np
-import message_filters
-from std_msgs.msg import String
 from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -18,7 +16,7 @@ class image_converter:
      self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
      
   def callback(self,data):
-     pub = rospy.Publisher("detect_face", Int32, queue_size=1)
+     pub1 = rospy.Publisher("detect_face", Int32, queue_size=1)
      pub2 = rospy.Publisher("number_of_face", Int32, queue_size=1)
      try:
        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -30,16 +28,20 @@ class image_converter:
 
      gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-     face = faceCascade.detectMultiScale(gray,1.1,3,minSize=(30,30))
+     face = faceCascade.detectMultiScale(gray,1.1,3,minSize=(50,50))
+
+     flag = 1
 
      if len(face) > 0:
        for rect in face:
          cv2.rectangle(cv_image, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 255, 255), thickness=2)
-       pub.publish(len(face))
        print("face")
        print('{0}人ご来店'.format(len(face)))
+       pub2.publish(len(face))
      else:
-       print("no face")
+       flag = 0
+     
+     pub1.publish(flag)
 
      cv2.imshow('detected_face', cv_image)
      cv2.waitKey(3)
